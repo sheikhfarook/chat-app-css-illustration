9ffcde73af681e102d730a58dcc884b794c9a9ba
Farook <mohammedfarook085@gmail.com>
2023-12-19 12:35:58 +0530

    background css is complete

--- src/components/css-design.jsx
+++ src/components/css-design.jsx
@@ -1,5 +1,16 @@
 const CssDesign = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="flex  w-full">
+        <div
+          className="w-[31rem] h-[50rem] fixed -left-[5rem]
+         bg-black rounded-b-full "></div>
+        <div
+          className="w-[31rem] h-[50rem] fixed -right-36
+         bg-black rounded-t-full mt-[165px] "></div>
+      </div>
+    </div>
+  );
 };
 
 export default CssDesign;
